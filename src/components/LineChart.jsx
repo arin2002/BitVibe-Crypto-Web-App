8572bf55e2e107ff516329158f94f5a5328b00e4
Arin Agarwal <arin.agarwal02@gmail.com>
2023-09-26 17:17:51 +0000

    Sep 26, 2023, 10:47 PM

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Line } from 'react-chartjs-2'
 import {Col,Row,Typography} from 'antd';
-import Chart from "chart.js/auto";
+// import Chart from "chart.js/auto";
 
 const {Title} = Typography;
 
